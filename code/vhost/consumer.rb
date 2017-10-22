79ac7eb8926c71af82e9ca12e18e034504ac3ae5
Gautam Manohar <gautam@The-Box.local>
2017-10-22 16:00:13 +0530

    first commit

--- code/vhost/consumer.rb
+++ code/vhost/consumer.rb
@@ -3,13 +3,13 @@
 require "bunny"
 
 conn = Bunny.new(:host        => "34.215.45.93",
-                 :vhost       => "mov",
+                 :vhost       => "movies",
                  :user        => "jerry",
                  :password    => "jerry")
 
 conn.start
 ch   = conn.create_channel
-q    = ch.queue("mov_logs", :durable => true)
+q    = ch.queue("movie_logs", :durable => true)
 begin
   puts " [*] Waiting for messages. To exit press CTRL+C"
   q.subscribe(:block => true) do |delivery_info, properties, body|
--- code/vhost/publisher.rb
+++ code/vhost/publisher.rb
@@ -3,16 +3,16 @@
 require "bunny"
 
 conn = Bunny.new(:host        => "34.215.45.93",
-                 :vhost       => "mov",
+                 :vhost       => "movies",
                  :user        => "jerry",
                  :password    => "jerry")
 
 conn.start
 ch   = conn.create_channel
-#q    = ch.queue("mov_logs", :durable => true)
-q    = ch.queue("mov_logs")
+#q    = ch.queue("movie_logs", :durable => true)
+q    = ch.queue("movie_logs")
 
-ch.default_exchange.publish("This is an mov message", :routing_key => q.name)
-puts " [x] Sent 'This is an mov message'"
+ch.default_exchange.publish("This is a movie message", :routing_key => q.name)
+puts " [x] Sent 'This is a movie message'"
 
 conn.close
