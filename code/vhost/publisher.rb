c4e49a913bb3946e40a71b260b42432d9f469d81
Gautam Manohar <gautam@The-Box.local>
2017-10-22 19:56:02 +0530

    first commit

--- code/vhost/publisher.rb
+++ code/vhost/publisher.rb
@@ -2,15 +2,15 @@
 # encoding: utf-8
 require "bunny"
 
-conn = Bunny.new(:host        => "34.215.45.93",
+conn = Bunny.new(:host        => "54.169.56.245",
                  :vhost       => "movies",
                  :user        => "jerry",
                  :password    => "jerry")
 
 conn.start
 ch   = conn.create_channel
-#q    = ch.queue("movie_logs", :durable => true)
-q    = ch.queue("movie_logs")
+q    = ch.queue("movie_logs", :durable => true)
+#q    = ch.queue("movie_logs")
 
 ch.default_exchange.publish("This is a movie message", :routing_key => q.name)
 puts " [x] Sent 'This is a movie message'"
