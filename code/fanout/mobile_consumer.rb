1e1b3297fcdbbc8c2947c1eceedbece77967a8af
Gautam Manohar <gautam@The-Box.local>
2017-10-23 22:41:42 +0800

    update

--- code/fanout/mobile_consumer.rb
+++ code/fanout/mobile_consumer.rb
@@ -2,16 +2,16 @@
 # encoding: utf-8
 require "bunny"
 
-conn = Bunny.new(:host        => "34.215.45.93",
-                 :vhost       => "mov",
+conn = Bunny.new(:host        => "54.169.56.245",
+                 :vhost       => "movies",
                  :user        => "jerry",
                  :password    => "jerry")
 
 conn.start
 ch   = conn.create_channel
-q    = ch.queue("ip_top_rev", :durable => true)
+q    = ch.queue("mobile_top_reviews", :durable => true)
 begin
-  puts " [*] Waiting for messages from ip_top_rev. To exit press CTRL+C"
+  puts " [*] Waiting for messages from mobile_top_reviews. To exit press CTRL+C"
   q.subscribe(:block => true) do |delivery_info, properties, body|
     puts " [x] Received #{body}"
   end
--- code/fanout/publisher.rb
+++ code/fanout/publisher.rb
@@ -2,16 +2,16 @@
 # encoding: utf-8
 require "bunny"
 
-conn = Bunny.new(:host        => "34.215.45.93",
-                 :vhost       => "mov",
+conn = Bunny.new(:host        => "54.169.56.245",
+                 :vhost       => "movies",
                  :user        => "jerry",
                  :password    => "jerry")
 
 conn.start
 ch   = conn.create_channel
-x    = ch.fanout("top_rev_exchange", :durable => true)
+x    = ch.fanout("top_reviews_exchange", :durable => true)
 
-x.publish("This is a top rev message")
-puts " [x] Sent 'This is a top rev message'"
+x.publish("This is a top reviews message")
+puts " [x] Sent 'This is a top reviews message'"
 
 conn.close
