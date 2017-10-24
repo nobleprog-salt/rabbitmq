cf6e3494b585c5419261a60759d511c2eec2e33a
Gautam Manohar <gautam@The-Box.local>
2017-10-24 14:56:25 +0800

    update

--- code/basic/publisher.rb
+++ code/basic/publisher.rb
@@ -2,13 +2,13 @@
 # encoding: utf-8
 require "bunny"
 
-conn = Bunny.new(:host        => "54.169.56.245",
+conn = Bunny.new(:host        => "54.169.201.143",
                  :user        => "admin",
                  :password    => "admin")
 
 conn.start
 ch   = conn.create_channel
-q    = ch.queue("first_que")
+q    = ch.queue("second_que", :durable => true)
 
 ch.default_exchange.publish("Hello World!", :routing_key => q.name)
 puts " [x] Sent 'Hello World!'"
